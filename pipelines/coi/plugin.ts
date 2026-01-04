2a226b1913ac5fb8afd55bb7ffe08a0013f1c04f
Linho <linho1219@outlook.com>
2026-01-04 14:34:36 +0800

    补充预览服务器配置

--- pipelines/coi/plugin.ts
+++ pipelines/coi/plugin.ts
@@ -23,6 +23,13 @@ function coiHeaderPlugin(): Plugin {
         next()
       })
     },
+    configurePreviewServer(server) {
+      server.middlewares.use((_req, res, next) => {
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+        next()
+      })
+    },
   }
 }
 
